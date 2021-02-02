059fa9a9574f06deb04f5a41efa92aca046886c3
Junaed Abed <junaedccpc17@gmail.com>
2021-02-02 14:58:03 +0600

    lifting up state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import './App.css';
 import React, { Component } from "react";
-// import ValidationForm from './components/ValidationForm';
-import FetchRandomUser from './components/FetchRandomUser';
+import Counter from './components/Counter';
 
 
 class App extends Component {
   
   state = {
-    visible: true,
+      count: 0
+  };
+  
+  increment = () => {
+      this.setState({
+          count: this.state.count + 1
+      });
+  };
+  
+  decrement = () => {
+    this.setState({
+        count: this.state.count - 1
+    });
   };
   
   render() {
     
     return (
       <div className="App">
-        <FetchRandomUser />
+        <Counter 
+          count={this.state.count} 
+          decrement={this.decrement} 
+          increment={this.increment} 
+        />
+        <Counter 
+          count={this.state.count} 
+          decrement={this.decrement} 
+          increment={this.increment} 
+        />
       </div>
     );
   }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,35 +10,14 @@ export default class Counter extends React.Component {
     //     };
     // }
     
-    state = {
-        count: 0
-    };
     
-    componentWillUnmount() {
-        console.log('unmoounting');
-    }
-    
-    componentDidMount() {
-        console.log('mounting');
-    }
-    
-    increment = () => {
-        this.setState({
-            count: this.state.count + 1
-        });
-    }
-    decrement = () => {
-        this.setState({
-            count: this.state.count - 1
-        });
-    }
     
     render() {
         return (
         <div>
-            <div>count: {this.state.count}</div>
-            <button onClick={this.increment}>increment</button>
-            <button onClick={this.decrement}>decrement</button>  
+            <div>count: {this.props.count}</div>
+            <button onClick={this.props.increment}>increment</button>
+            <button onClick={this.props.decrement}>decrement</button>  
         </div>
         );
     }

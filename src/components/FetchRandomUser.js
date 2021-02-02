025ff251b49df8451a993387d257dcb0a2b2910d
Junaed Abed <junaedccpc17@gmail.com>
2021-02-02 14:46:53 +0600

    redering array

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -4,14 +4,14 @@ export default class FetchRandomUser extends React.Component{
     
     state ={
         loading: true,
-        person: null,
+        people: null,
     }
     
     async componentDidMount(){
-        const url = "https://api.randomuser.me/";
+        const url = "https://api.randomuser.me/?results=5";
         const res = await fetch(url);
         const data = await res.json();
-        this.setState({person: data.results[0], loading: false });
+        this.setState({people: data.results, loading: false });
     }
     
     render(){
@@ -19,17 +19,37 @@ export default class FetchRandomUser extends React.Component{
             return <div>loading...</div>
         }
         
-        if(!this.state.person){
+        if(!this.state.people.length){
             return <div>did not get a person</div>
         }
         
+        const peopleJsx= this.state.people.map(person => (
+                    <div key={person.login.uuid + person.name.last}>
+                        <div>{person.name.title}</div>
+                        <div>{person.name.first}</div>
+                        <div>{person.name.last}</div>
+                        <div>{person.location.city}</div>
+                        <img src={person.picture.large} />
+                    </div>
+                ));
+        
+        // const peopleJsx =  [];
+        
+        // this.state.people.forEach((person) => {
+        //     peopleJsx.push(
+        //         <div key={person.login.uuid + person.name.last}>
+        //             <div>{person.name.title}</div>
+        //             <div>{person.name.first}</div>
+        //             <div>{person.name.last}</div>
+        //             <div>{person.location.city}</div>
+        //             <img src={person.picture.large} />
+        //         </div>
+        //     )
+        //     });
+        
         return (
             <div>
-                <div>{this.state.person.name.title}</div>
-                <div>{this.state.person.name.first}</div>
-                <div>{this.state.person.name.last}</div>
-                <div>{this.state.person.location.city}</div>
-                <img src={this.state.person.picture.large} />
+                {peopleJsx}
             </div>
         );
         
